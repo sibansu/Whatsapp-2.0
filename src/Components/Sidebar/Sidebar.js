23e3345c7ef50efce52191175cc9275d25bfc2a7
Sibansu <mishrasibansu16@gmail.com>
2023-02-09 22:10:26 +0530

    sidebar added

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,42 +4,45 @@ import "./Sidebar.css"
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import FilterListIcon from '@mui/icons-material/FilterList';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { IconButton, Avatar } from '@mui/material';
 import SidebarChat from '../SidebarChat/SidebarChat';
 function Sidebar() {
-  return (
-    <div className="Sidebar">
-        <div className="sidebar_header">
-            <Avatar></Avatar>
-            <div className="sidebar_header_right">
+    return (
+        <div className="Sidebar">
+            <div className="sidebar_header">
+                <Avatar></Avatar>
+                <div className="sidebar_header_right">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar_search">
+                {/* <input type="text" name="" id="" /> */}
+                <div className="sidebar_searchContainer">
+                    <SearchOutlinedIcon />
+                    <input type="text" name="" placeholder='Search or start a new chat' id="" />
+                </div>
+                {/* <SearchOutlinedIcon></SearchOutlinedIcon> */}
                 <IconButton>
-                    <DonutLargeIcon />
-                </IconButton>
-                <IconButton>
-                    <ChatIcon />
-                </IconButton>
-                <IconButton>
-                    <MoreVertIcon />
+                    <FilterListIcon />
                 </IconButton>
             </div>
+            <div className="sidebar_chats">
+                <SidebarChat></SidebarChat>
+                <SidebarChat></SidebarChat>
+                <SidebarChat></SidebarChat>
+            </div>
         </div>
-        <div className="sidebar_search">
-             {/* <input type="text" name="" id="" /> */}
-             <div className="sidebar_searchContainer">
-                <SearchOutlinedIcon /> 
-                <input type="text" name="" placeholder='Search or start a new chat' id="" />
-             </div>
-        </div>
-        <div className="search_chats">
-            <SidebarChat></SidebarChat>
-            <SidebarChat></SidebarChat>
-            <SidebarChat></SidebarChat>
-            <SidebarChat></SidebarChat>
-            <SidebarChat></SidebarChat>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
--- src/Components/SidebarChat/SidebarChat.js
+++ src/Components/SidebarChat/SidebarChat.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./SidebarChat.css"
+import { Avatar } from '@mui/material'
 
 function SidebarChat() {
   return (
-    <div className='sidebarChat'>SidebarChat</div>
+    <div className='sidebarChat'>
+      <Avatar></Avatar>
+      <div className="sidebarChat_info">
+        <h1>Name</h1>
+        <p>This is Chat</p>
+      </div>
+    </div>
   )
 }
 
